0b3e9e93e585c230aa6e1aacc95c9a7c7ef8400f
Sefa Eyeoglu <contact@scrumplex.net>
2023-02-20 14:17:26 +0100

    chore: add flake
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Discord bot for Prism Launcher";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = { nixfmt.enable = true; };
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          packages = with pkgs; [ nodePackages.pnpm ];
+        };
+      });
+}
